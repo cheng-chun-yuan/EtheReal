39537e6a186dfdb18265c4539c7750d48862d239
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2024-06-28 19:05:28 +0800

    feat: EtheRael Version-1

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,15 +3,15 @@ pragma solidity >=0.8.21 <0.9.0;
 
 import { BaseScript } from "./Base.s.sol";
 
-import { MockERC20 } from "../src/MockERC20.sol";
-import { XueMembershipNFT } from "../src/XueMembershipNFT.sol";
+import { EtheReal } from "../src/EtheReal.sol";
+import { HouseAsset } from "../src/HouseAsset.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast returns (MockERC20 token, XueMembershipNFT nft) {
-        token = new MockERC20();
-        nft = new XueMembershipNFT("https://xuedaoisgood.com/");
+    function run() public broadcast returns (EtheReal etheReal, HouseAsset nft) {
+        nft = new HouseAsset();
+        etheReal = new EtheReal(address(nft));
 
-        return (token, nft);
+        return (etheReal, nft);
     }
 }
